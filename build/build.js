71930c7deec83277d545ee22e0cf0662f4d5c09b
zhazheng <512557403@qq.com>
2018-09-25 16:52:17 +0800

    feat: init project with version 1.0.0

---
+++ build/build.js
@@ -0,0 +1,38 @@
+const babel = require('rollup-plugin-babel')
+const replace = require('rollup-plugin-replace')
+const minify = require('rollup-plugin-babel-minify')
+const rollup = require('rollup')
+const version = process.env.VERSION || require('../package.json').version
+
+const inputOptions = {
+  input: 'src/index.js',
+  plugins: [
+    replace({
+      [`process.env.NODE_ENV`]: process.env.NODE_ENV,
+      __VERSION__: version,
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    minify(),
+  ]
+}
+
+const outputOptions = {
+  name: 'WechatShare',
+  file: './dist/index.js',
+  format: 'umd',
+}
+
+const build = async function() {
+  const bundle = await rollup.rollup(inputOptions)
+
+  const {
+    code,
+    map,
+  } = await bundle.generate(outputOptions)
+
+  await bundle.write(outputOptions)
+}
+
+build()
