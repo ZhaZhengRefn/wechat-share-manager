a65c27974f7fd342ca198a2c94fb89c7e2bdd154
zhazheng <512557403@qq.com>
2018-10-29 10:06:21 +0800

    feat: add typescript loader & example page

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,8 @@
 const babel = require('rollup-plugin-babel')
 const replace = require('rollup-plugin-replace')
 const minify = require('rollup-plugin-babel-minify')
+const rollupTypescript = require('rollup-plugin-typescript')
+const typescript = require('typescript');
 const rollup = require('rollup')
 const version = process.env.VERSION || require('../package.json').version
 
@@ -11,9 +13,12 @@ const inputOptions = {
       [`process.env.NODE_ENV`]: process.env.NODE_ENV,
       __VERSION__: version,
     }),
-    babel({
-      exclude: 'node_modules/**',
+    rollupTypescript({
+      typescript,
     }),
+    // babel({
+    //   exclude: 'node_modules/**',
+    // }),
     minify(),
   ]
 }
